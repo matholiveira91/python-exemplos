b2d71da7bd38bd5621d2952a63d9678c047a5046
mth <matheusoliveiratux4me@gmail.com>
2021-11-28 21:42:08 -0300

    :tada: working with text archives

--- wc.py
+++ wc.py
@@ -3,19 +3,22 @@
 import numpy as np
 from PIL import Image
 from wordcloud import WordCloud,STOPWORDS
-import matplotlib.pyplot as plt
-import tkinter
+import matplotlib.pyplot as plt 
+
 #REFERENCES INDIAN PYTHONISTA
 #REFERENCES UMANGAHUJA1
 def create_wc(text):
-    mask = np.array(Image.open('cloud.jpg'))
+    mask = np.array(Image.open('mobi.png'))
     stopwords = set(STOPWORDS)
     wc = WordCloud(background_color="white", mask = mask).generate(text)
     plt.imshow(wc)
     plt.axis('off')
-    plt.show()
+    #plt.show()
+    
+    wc.to_file("wc.png")
+   
        
     
-texto="Somos uma organização regional sem fins lucrativos do Rio Grande do Norte. Temos a finalidade de reunir toda a comunidade ao redor de eventos que enriqueçam nosso trabalho e nosso conhecimento, fornecendo assim um canal direto com profissionais da área."
-
-create_wc(texto)
+at=open('LICENSE','r')
+for t in  at:
+    create_wc(t)
